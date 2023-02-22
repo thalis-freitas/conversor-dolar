385ca6befb15994b32c73f85058c2019dcdd36f6
Thalis Freitas <ofthalis@gmail.com>
2023-02-21 22:33:04 -0300

    refactor: remove unnecessary variables

--- js/script.js
+++ js/script.js
@@ -1,102 +1,54 @@
-let date = document.getElementById("date")
-let dataAtual = new Date()
+'use strict'
 
-var dias = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
+let date = document.getElementById('date')
+let currentDate = new Date()
+let days = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+              'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+let weekday = days[currentDate.getDay()]
+let monthday = currentDate.getDate()
+let month = months[currentDate.getMonth()]
+let year = currentDate.getFullYear()
+let hours = currentDate.getHours()
+let minutes = currentDate.getMinutes()
 
-var meses = [
-    "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
-]
+if (minutes < 10) minutes = '0' + minutes
+if (hours < 10) hours = '0' + hours
+if (monthday < 10) monthday = '0' + monthday
+date.innerHTML = `${weekday}, ${monthday} de ${month} de ${year}, ${hours}:${minutes}`
 
-let diaSemana = dias[dataAtual.getDay()]
-let diaMes = dataAtual.getDate()
-let mes = meses[dataAtual.getMonth()]
-let ano = dataAtual.getFullYear()
-let horas = dataAtual.getHours()
-let minutos = dataAtual.getMinutes()
+let url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL'
+let dollarToday = document.getElementById('dollar-today')
+let dollar = new Number
 
-if (minutos < 10) minutos = "0" + minutos
-if (minutos == 0) minutos = "00"
-if (horas < 10 && horas > 0) horas = "0" + horas
-if (horas == 0) horas = "00"
-if (diaMes < 10) diaMes = "0" + diaMes
+fetch(url)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        dollar = parseFloat(data.USDBRL.ask).toFixed(2)
+        dollarToday.innerHTML = `Dólar hoje = R$${dollar}`
+    })
+    .catch(() => {  
+        dollarToday.innerHTML = `Ops, ocorreu um erro...`}
+    )
 
-date.innerHTML = ` ${diaSemana}, ${diaMes} de ${mes} de ${ano}, ${horas}:${minutos}`
+document.querySelector('#convert-to-real').addEventListener('click', ()=>{ 
+    let valor = document.getElementById('input-dollar').value
+    let resultado = document.getElementById('real-result')
+    if (valor <= 0) {
+        alert('Por favor insira um valor positivo para converter')
+    }else {
+        resultado.innerHTML = ` $${valor} = R$${(valor * dollar).toFixed(2)}`
+    }
+})
 
-let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL"
-
-let dolarHoje = document.getElementById("dollar-today")
-
-onload = () => {
-
-    fetch(url)
-        .then((response) => {
-            return response.json()
-        })
-        .then((dados) => {
-            let respostaDolar = (dados.USDBRL.ask)
-            let dolar = parseFloat(respostaDolar).toFixed(2)
-            dolarHoje.innerHTML = `Dólar hoje = R$${dolar}`
-        })
-        .catch(() => {
-            dolarHoje.innerHTML = `Ops, ocorreu um erro...`})
-}
-
-function converterParaReal() {
-
-    let inputDolar = document.getElementById("input-dollar")
-    let valor = inputDolar.value
-
-    fetch(url)
-        .then((response) => {
-            return response.json()
-        })
-        .then((dados) => {
-            let respostaDolar = (dados.USDBRL.ask)
-            let dolar = parseFloat(respostaDolar).toFixed(2)
-
-            let resultado = document.getElementById("real-result")
-            let valorConvertido = valor * dolar
-
-            if (valor <= 0) {
-                alert("Por favor insira um valor positivo para converter")
-            }
-            else {
-                conteudoResultado = ` $${valor} = R$${(valorConvertido).toFixed(2)}`
-                resultado.innerHTML = conteudoResultado
-            }
-        }
-        )
-}
-
-function converterParaDolar() {
-
-    let inputReal = document.getElementById("input-real")
-    let valor = inputReal.value
-
-    fetch(url)
-        .then((response) => {
-            return response.json()
-        })
-        .then((dados) => {
-            let respostaDolar = (dados.USDBRL.ask)
-            let dolar = parseFloat(respostaDolar).toFixed(2)
-
-            let resultado = document.getElementById("dollar-result")
-            let valorConvertido = valor / dolar
-
-            if (valor <= 0) {
-                alert("Por favor insira um valor positivo para converter")
-            } else {
-                conteudoResultado = `R$${valor} = $${(valorConvertido).toFixed(2)}`
-                resultado.innerHTML = conteudoResultado
-            }
-        }
-        )
-
-}
-
-let btnConverterParaReal = document.getElementById("convert-to-real")
-btnConverterParaReal.addEventListener("click", converterParaReal)
-
-let btnConverterParaDolar = document.getElementById("convert-to-dollar")
-btnConverterParaDolar.addEventListener("click", converterParaDolar)
+document.querySelector('#convert-to-dollar').addEventListener('click', ()=>{
+    let valor = document.getElementById('input-real').value
+    let resultado = document.getElementById('dollar-result')
+    if (valor <= 0) {
+        alert('Por favor insira um valor positivo para converter')
+    } else {
+        resultado.innerHTML = `R$${valor} = $${(valor / dollar).toFixed(2)}`
+    }
+})
